c1153819303909f26945dbe25c033698aaf20004
Buasad <hamoodat@live.com>
2020-11-09 02:13:56 +0300

    musica

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
+    
+    
+    
+    
+    @IBOutlet weak var ButtonName: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: Any) {
-        print("Aalya is luv and is my galb🥰")
-    }
-    
-    
+        ButtonName.setTitle("MaNigga", for: .normal)
+ 
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+                try AVAudioSession.sharedInstance().setActive(true)
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                guard let player = player else { return }
+
+                player.play(
+                )
+            } catch let error{
+                print(error.localizedDescription)
+            }
 
 }
 
+
+}
